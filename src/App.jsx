f36bf4f85b4dfd748eaba34bae7f498073683db5
Umasri537 <gubbalaumasri4@gmail.com>
2025-10-15 12:01:23 +0530

    Installation and Separation Using Vite in React

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Footer from "./Footer/Footer"
+import Header from "./Header/Header"
+import MainArea from "./MainAreaa/MainArea"
+
+
+function App() {
+    return (
+      <div style={{border:"2"}}>
+        {/* <h1>This is My React Component </h1> */}
+        <Header></Header>
+        <MainArea></MainArea>
+        <Footer></Footer>
+      </div>
+  )
+}
+
+export default App
